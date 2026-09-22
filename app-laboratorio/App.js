231ce99c5a3263cc4ffe5f9f40d438cb05c1ecb2
hayatodofreefire <hayat.azam@escola.pr.gov.br>
2026-09-22 20:46:31 +0000

    exe 4 feito

--- app-laboratorio/App.js
+++ app-laboratorio/App.js
@@ -4,7 +4,13 @@ const eu = {
   nome: 'Hayat Rossi Azam',
   idade: 16,
   cidade: 'Cascavel',
+  nota1: 8,
+  nota2: 7,
 };
+
+function calcularMedia(pessoa) {
+  return (pessoa.nota1 + pessoa.nota2) / 2;
+}
 
 export default function App() {
   return (
@@ -12,6 +18,7 @@ export default function App() {
       <Text>{eu.nome}</Text>
       <Text>{eu.idade}</Text>
       <Text>{eu.cidade}</Text>
+      <Text>{calcularMedia(eu)}</Text>
     </View>
   );
 }
@@ -19,7 +26,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#7e75f7',
+    backgroundColor: '#4592eb',
     alignItems: 'center',
     justifyContent: 'center',
   },
