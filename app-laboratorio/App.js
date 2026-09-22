1da3a60234280dcba5a3edf3a775208e97242009
hayatodofreefire <hayat.azam@escola.pr.gov.br>
2026-09-22 20:55:01 +0000

    exe 7 feito

--- app-laboratorio/App.js
+++ app-laboratorio/App.js
@@ -1,33 +1,39 @@
 import { StyleSheet, Text, View } from 'react-native';
 
 const eu = {
-  nome: 'Hayat Rossi Azam',
+  nome: 'Hayat',
   idade: 16,
   cidade: 'Cascavel',
   nota1: 8,
   nota2: 7,
 };
 
-function calcularMedia(pessoa) {
-  return (pessoa.nota1 + pessoa.nota2) / 2;
+function calcularMedia(aluno) {
+  return (aluno.nota1 + aluno.nota2) / 2;
 }
 
-function verificarSituacao(media) {
+function situacao(media) {
   if (media >= 6) {
     return 'Aprovado';
-  } else {
-    return 'Em recuperacao';
   }
+
+  return 'Em recuperacao';
 }
 
 export default function App() {
+  const media = calcularMedia(eu);
+
   return (
     <View style={styles.container}>
-      <Text>{eu.nome}
-      <Text>{eu.idade}</Text>
-      <Text>{eu.cidade}</Text>
-      <Text>{calcularMedia(eu)}</Text>
-      <Text>{verificarSituacao(calcularMedia(eu))}</Text>
+      <Text style={styles.titulo}>Meu aplicativo</Text>
+
+      <Text>Nome: {eu.nome}</Text>
+      <Text>Idade: {eu.idade}</Text>
+      <Text>Cidade: {eu.cidade}</Text>
+      <Text>Nota 1: {eu.nota1}</Text>
+      <Text>Nota 2: {eu.nota2}</Text>
+      <Text>Média: {media}</Text>
+      <Text>Situação: {situacao(media)}</Text>
     </View>
   );
 }
@@ -35,8 +41,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#5f1cca',
+    backgroundColor: '#5c23f8ce',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+
+  titulo: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
   },
 });
